d028bda4b267d934f9ba874e67d56f666bb63491
boyoungkk33 <boyoungkk33@naver.com>
2023-06-14 13:51:08 +0900

    수정,삭제

--- ex43-2.js
+++ ex43-2.js
@@ -1,31 +1,34 @@
-const schedule =require('node-schedule');
-const { start } = require('repl');
+const schedule = require('node-schedule');
 
-const j = schedule.scheduleJob'00 30 11 **1-5, () =>{
+const j = schedule.scheduleJob('00 30 11 * * 1-5', () => {
     console.log('Cron-style scheduling');
 });
 
-const rule=new schedule.RecurrenceRule();
-rule.dayOfWeek=[0, new schedule.Range(0,6)];
-rule.hour=17;
-rule.minute=17;
-const k =schedule.scheduleJob(rule, ()=>{
-     console.log('Recurrence Rule Scheduling');
+const rule = new schedule.RecurrenceRule();
+rule.dayOfWeek = [0, new schedule.Range(0, 6)];
+rule.hour = 17;
+rule.minute = 17;
+const k = schedule.scheduleJob(rule, () => {
+    console.log('Recurrence Rule Scheduling');
 });
 
-const l =schdule.scheduleJob(
-    {
-        hour:21, minute:10, dayOfWeek:6}, ()=>{
-          console.log('Object Literal Syntax');
-        }
+const l = schedule.scheduleJob(
+    { hour: 21, minute: 10, dayOfWeek: 6 },
+    () => {
+        console.log('Object Literal Syntax');
+    }
 );
 
-const startTime = new Date(Date.now()+5000);
+const startTime = new Date(Date.now() + 5000);
 const endTime = new Date(startTime.getTime() + 5000);
 
-const m=schedule.scheduleJob(
-{
-    start:startTime, end:endTime, rule:'*/1*****'}, ()=>{
-    console.log('Set startTime and EndTime');
-}
-)
+const m = schedule.scheduleJob(
+    {
+        start: startTime,
+        end: endTime,
+        rule: '*/1 * * * * *'
+    },
+    () => {
+        console.log('Set startTime and endTime');
+    }
+);
--- ex49-1.js
+++ ex49-1.js
@@ -1,13 +1,81 @@
-const mysql =require('mysql');
-const express =require('express');
-const fs=require('fs');
-const ejs=require('ejs');
-const bodyParser =require('body-parser');
+const mysql = require('mysql');
+const express = require('express');
+const fs = require('fs');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
 
-const connrection=mysql.createConnection({
-host:'localhost',
-user:'root',
-password:'1234',
-database:'book_table1',
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '1234',
+    database: 'book_table1',
+    port: '3306'
+});
 
+const app = express();
+app.use(bodyParser.urlencoded({
+    extended: false,
+}));
+
+app.listen(2000, () => {
+    console.log('Server is running at http://127.0.0.1:2000');
+    connection.connect();
+});
+
+app.get('/', (request, response) => {
+    fs.readFile('bookList.html', 'utf-8', (error, data) => {
+        connection.query('SELECT * FROM books', (error, results) => {
+            if (error) throw error;
+            response.send(ejs.render(data, {
+                data: results
+            }));
+        });
+    });
+});
+
+// Add new book
+app.get('/create', (request, response) => {
+    fs.readFile('insertNewBook.html', 'utf-8', (error, data) => {
+        if (error) throw error;
+        response.send(data);
+    });
+});
+
+app.post('/create', (request, response) => {
+    const body = request.body;
+    connection.query('INSERT INTO books (genre, name, writer, releasedate) VALUES (?, ?, ?, ?)',
+        [body.genre, body.name, body.writer, body.releasedate], (error, results) => {
+            if (error) throw error;
+            response.redirect('/');
+        });
+});
+
+// Modify book
+app.get('/modify/:id', (request, response) => {
+    fs.readFile('modify.html', 'utf-8', (error, data) => {
+        if (error) throw error;
+        connection.query('SELECT * FROM books WHERE number = ?', [request.params.id], (error, results) => {
+            if (error) throw error;
+            response.send(ejs.render(data, {
+                data: results[0]
+            }));
+        });
+    });
+});
+
+app.post('/modify/:id', (request, response) => {
+    const body = request.body;
+    connection.query('UPDATE books SET genre = ?, name = ?, writer = ?, releasedate = ? WHERE number = ?',
+        [body.genre, body.name, body.writer, body.releasedate, request.params.id], (error, results) => {
+            if (error) throw error;
+            response.redirect('/');
+        });
+});
+
+// Delete book
+app.get('/delete/:id', (request, response) => {
+    connection.query('DELETE FROM books WHERE number = ?', [request.params.id], (error, results) => {
+        if (error) throw error;
+        response.redirect('/');
+    });
 });
